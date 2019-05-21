17e770dc6af933df8d424de8e8b110eb02512a1f
Ghalistan <gghalistan@gmail.com>
2019-05-21 19:39:34 +0700

    Update Dependency

--- Migrations/20190521123718_Initial.Designer.cs
+++ Migrations/20190521123718_Initial.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Kemenkeu.Migrations
 {
     [DbContext(typeof(KemenkeuContext))]
-    [Migration("20190521120526_Initial")]
+    [Migration("20190521123718_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
